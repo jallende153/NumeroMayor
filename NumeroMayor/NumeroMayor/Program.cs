f7d7e90c58b516a6508852be1c5b6337a9790f45
Joel Lopez <jallende153@gmail.com>
2017-10-06 13:49:36 -0500

    programa que calcula el mayor de tres números

--- NumeroMayor/NumeroMayor/Program.cs
+++ NumeroMayor/NumeroMayor/Program.cs
@@ -10,6 +10,29 @@ namespace NumeroMayor
     {
         static void Main(string[] args)
         {
+            int num1, num2, num3;
+            Console.Write("Ingrese el primer número: ");
+            num1 = Convert.ToInt32( Console.ReadLine());
+            Console.Write("Ingrese el segundo número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el tercer número: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("El número mayor {0}", num1);
+            }
+            else
+            {
+                if(num2 > num1 && num2 > num3)
+                {
+                    Console.WriteLine("El número mayor {0}", num2);
+                }
+                else
+                {
+                    Console.WriteLine("El número mayor {0}", num3);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
